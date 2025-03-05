f1cd7fb36331bc2eabdd0942bd4f6959b988e983
noemie-mbg <n.mbongo@icloud.com>
2025-03-05 15:05:05 +0100

    Create falcon_signature.h

---
+++ Signature/falcon_signature.h
@@ -0,0 +1,13 @@
+#ifndef Falcon_SIGN_H  // Protection contre l'inclusion multiple
+#define Falcon_SIGN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "falcon.h"
+
+int falcon_sign(); //Déclaration de la fonction
+
+#endif
